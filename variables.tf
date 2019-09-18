54537a52052b6787b5ab8ea0be6e5368dc3e5fbd
Poojanakaskar <pooja.naksskar@gmail.com>
2019-09-18 13:46:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "cluster_identifier" {
+  default = ""
+}
+variable "engine" {
+  default = ""
+}
+variable "engine_version" {
+  default = ""
+}
+variable "database_name" {
+  default = ""
+}
+
+variable "master_username" {
+  default = ""
+}
+variable "master_password" {
+  default = ""
+}
+variable "backup_retention_period" {
+  default = ""
+}
+variable "preferred_backup_window" {
+  default = ""
+}
+variable "availability_zones" {
+  default = []
+}
