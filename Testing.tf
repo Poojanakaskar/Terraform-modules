663a1730c322b583eaeb36b3bf4607e26f5ddbe1
Poojanakaskar <pooja.naksskar@gmail.com>
2020-10-05 13:15:10 +0530

    Update Testing.tf

--- Testing.tf
+++ Testing.tf
@@ -2,6 +2,7 @@ provider "aws"{
     region = "us-east-1"
     access_key = "AKIAR54SMTXBD6WWWSG6"
     secret_key = "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"
+    version = "~> 1.11"
 }
 resource "aws_s3_bucket" "azdo"{
     bucket = "myazbucket3"
